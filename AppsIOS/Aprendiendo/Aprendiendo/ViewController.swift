5ea1794cba79977492e62484dc86db7de9c92784
ana <anamaceda@MacBook-Air.local>
2020-06-18 14:12:03 -0500

    MainStoryBoard, Assets.xcasset, Librería de objetos ios, Content Mode(Aspect Fit, Scale To Fill, Aspect Fill,). Button, TextField, ImageView, View, Propiedades (Background, PlaceHolder, Font)

---
+++ AppsIOS/Aprendiendo/Aprendiendo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aprendiendo
+//
+//  Created by ana on 18/06/20.
+//  Copyright © 2020 martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

a7840d22a17e75b4c2b3d277a3f6f4c8f5508e1c
ana <anamaceda@MacBook-Air.local>
2020-06-18 23:51:28 -0500

    Utilizando controles en código: Class View Controller, IBOutlet, IBAction, Storyboar Reference: Identifier, performSegue()

--- AppsIOS/Aprendiendo/Aprendiendo/ViewController.swift
+++ AppsIOS/Aprendiendo/Aprendiendo/ViewController.swift
@@ -9,6 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //MARK: - Referencias UI
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextFIeld: UITextField!
+    
+    @IBAction func loginButtonAction() {
+        //1. Obtener los valores de los TextField
+        let email = emailTextField.text
+        let password = passwordTextFIeld.text
+        
+        if email == "martino@gmail.com" && password == "1234" {
+            print("Iniciando sesion")
+            /*Utilizamos performSegue para movernos desde el flujo de nuestro
+             Main.storyboard hacia el Home.storyboard, el primer parametro es
+             el identificador del Segue que va al Storyboard Reference el cual
+             conecta con Home.storyboard
+             */
+            performSegue(withIdentifier: "home_segue", sender: nil)
+        } else {
+            print("contraseña o cuenta invalida")
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
